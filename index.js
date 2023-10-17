23d9cc1e947e59db7416c0a039777e885c227bd6
Mercy Cherono <cheronom142@gmail.com>
2023-10-17 16:30:31 +0800

    Add to cart function

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+let listCard = document.querySelector(".listCard");
+let total = document.querySelector(".total");
+let quantity = document.querySelector(".quantity");
+let listCards = [];
+let products = [];
+
 document.addEventListener("DOMContentLoaded", function(){
 
 let startShopping = document.querySelector(".shopping");
 let closeShopping = document.querySelector(".closeShopping");
 let list = document.querySelector(".list");
-let listCard = document.querySelector(".listCard");
+
 let body = document.querySelector("body");
-let total = document.querySelector(".total");
-let quantity = document.querySelector(".quantity");
+
 
 startShopping.addEventListener("click", () => {
     body.classList.add("active");
@@ -16,7 +21,7 @@ closeShopping.addEventListener("click", () => {
     body.classList.remove("active");
 })
 
-let listCards = [];
+
 
 
 
@@ -26,11 +31,18 @@ let listCards = [];
 function getProducts(){
     fetch("https://fakestoreapi.com/products")
     .then((response) => response.json())
-    .then((data) => displayProducts(data));
+    .then((data) => {
+        console.log(data);
+        products = data;
+        console.log(products);
+        displayProducts();
+    }); 
+    
 }
 
+
 //Function to display our products 
-function displayProducts (products) {
+function displayProducts () {
   products.forEach((value, key) => {
     let newDiv = document.createElement("div");
     newDiv.classList.add("item");
@@ -38,7 +50,7 @@ function displayProducts (products) {
        <img src="${value.image}"/>
        <div class="title">${value.title}</div>
        <div class="price">$${value.price}</div>
-       <button onclick="addToCard(${key})">Add to Cart</button>
+       <button onclick="addToCart(${key})">Add to Cart</button>
     `;
     list.appendChild(newDiv);
   })
@@ -46,6 +58,44 @@ function displayProducts (products) {
 getProducts();
 
 });
+
+//Function to add items to cart
+function addToCart(key){
+    if (listCards[key] == null){
+        listCards[key] = products[key];
+        listCards[key].quantity = 1;
+    }
+    reloadCart();
+}
+
+function reloadCart(){
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+
+        if (value!= null){
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+            <div><img src="${value.image}"/></div>
+            <div>${value.title}</div>
+            <div>${value.quantity}</div>
+            <div>
+            <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+            <div class="count">${value.quantity}</div>
+            <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+            </div>
+            
+            `;
+            listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = totalPrice;
+    quantity.innerText = count;
+}
+
 
 
 
